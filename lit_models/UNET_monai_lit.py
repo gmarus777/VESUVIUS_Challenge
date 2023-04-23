e5b8cc9acc8636e8a253bf3b2b1dffbf8a8abe6f
gregory <txe16@psu.edu>
2023-04-23 16:14:02 -0400

    added augmentatios

--- lit_models/UNET_monai_lit.py
+++ lit_models/UNET_monai_lit.py
@@ -55,7 +55,7 @@ class UNET_lit(pl.LightningModule):
             spatial_dims=2,
             in_channels= self.z_dim,
             out_channels=1,
-            channels=(64, 128, 256, 512, 1024),
+            channels=(32, 64, 128, 256, 512,),
             strides=(2, 2, 2, 2, ),
             num_res_units=4,
             dropout=0,
