c8026cac4ce96ff6b00bc0ada8a7b592d1c3a303
gregory <txe16@psu.edu>
2023-04-30 16:36:33 -0400

    changed to smp loss

--- lit_models/UNET_monai_lit.py
+++ lit_models/UNET_monai_lit.py
@@ -147,7 +147,7 @@ class UNET_lit(pl.LightningModule):
     def criterion(self, y_pred, y_true, mask):
         #return  0.5*self.loss_bce(y_pred, y_true) +  self.loss_dice(y_pred, y_true) #+ 2*self.loss_focal(y_pred, y_true)
         #return self.loss_bce(y_pred, y_true) +  self.loss_dice(y_pred, y_true,) +  self.loss_focal(y_pred, y_true)
-        return 2.5*self.loss_focal(y_pred*mask, y_true) +self.loss_dice(y_pred*mask, y_true)
+        return 2*self.loss_focal(y_pred*mask, y_true) +self.loss_dice(y_pred*mask, y_true)
 
 
     def combined_loss(self, pred, label, mask):
