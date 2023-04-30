9b3a1f8b2a70b583e7c8237f5d333c4093a8ea40
gregory <txe16@psu.edu>
2023-04-29 20:41:47 -0400

    changed to dice +bce loss

--- lit_models/UNET_monai_lit.py
+++ lit_models/UNET_monai_lit.py
@@ -215,7 +215,7 @@ class UNET_lit(pl.LightningModule):
 
 
         self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("monitor", monitor, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("monitor", monitor.item(), on_step=False, on_epoch=True, prog_bar=True)
         self.log("accuracy", accuracy.item(), on_step=False, on_epoch=True, prog_bar=True)
         self.log("recall", recall.item(), on_step=False, on_epoch=True, prog_bar=True)
         self.log("precision", precision.item(), on_step=False, on_epoch=True, prog_bar=True)
