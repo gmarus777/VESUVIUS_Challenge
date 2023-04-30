8b8e0adceedc8e70f379b1db5382c8957e021d4b
gregory <txe16@psu.edu>
2023-04-30 16:33:06 -0400

    changed to smp loss

--- lit_models/UNET_monai_lit.py
+++ lit_models/UNET_monai_lit.py
@@ -114,7 +114,7 @@ class UNET_lit(pl.LightningModule):
         self.loss_bce = smp.losses.SoftBCEWithLogitsLoss()
         self.loss_focal = smp.losses.FocalLoss(
                                 mode = 'binary',
-                                  alpha=.25,
+                                  #alpha=.1,
                                   gamma=2.0,
                                   ignore_index=None,
 
@@ -147,7 +147,7 @@ class UNET_lit(pl.LightningModule):
     def criterion(self, y_pred, y_true, mask):
         #return  0.5*self.loss_bce(y_pred, y_true) +  self.loss_dice(y_pred, y_true) #+ 2*self.loss_focal(y_pred, y_true)
         #return self.loss_bce(y_pred, y_true) +  self.loss_dice(y_pred, y_true,) +  self.loss_focal(y_pred, y_true)
-        return 10*self.loss_focal(y_pred*mask, y_true) +self.loss_dice(y_pred*mask, y_true)
+        return self.loss_focal(y_pred*mask, y_true) +self.loss_dice(y_pred*mask, y_true)
 
 
     def combined_loss(self, pred, label, mask):
@@ -181,7 +181,7 @@ class UNET_lit(pl.LightningModule):
             in_channels= self.z_dim,
             out_channels=1,
             channels=(  32, 64, 128, 256, 512, 1024 ),
-            strides=(2, 2, 2, 2,2 ),
+            strides=(2, 2, 2, 2, 2),
             num_res_units=2,
             dropout=0,
             norm = 'batch',
