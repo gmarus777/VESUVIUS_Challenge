c04452eb0960576482288ff44c3ec7b0087ed073
gregory <txe16@psu.edu>
2023-05-07 13:06:41 -0400

    initial push

--- Data_Modules/Vesuvius_Dataset.py
+++ Data_Modules/Vesuvius_Dataset.py
@@ -1,7 +1,5 @@
 import segmentation_models_pytorch as smp
 
-
-
 from pathlib import Path
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
@@ -14,20 +12,18 @@ from albumentations import ImageOnlyTransform
 import pytorch_lightning as pl
 from tqdm.auto import tqdm
 
-
-PATCH_SIZE =224
+PATCH_SIZE = 224
 Z_DIM = 24
 
-
 PATH = Path().resolve().parents[0]
-#DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 KAGGLE_DIR = PATH / "kaggle"
 
 INPUT_DIR = KAGGLE_DIR / "input"
 
 COMPETITION_DATA_DIR = INPUT_DIR / "vesuvius-challenge-ink-detection"
-COMPETITION_DATA_DIR_str =  "kaggle/input/vesuvius-challenge-ink-detection/"
+COMPETITION_DATA_DIR_str = "kaggle/input/vesuvius-challenge-ink-detection/"
 
 TRAIN_DATA_CSV_PATH = COMPETITION_DATA_DIR / "data_train_1.csv"
 TEST_DATA_CSV_PATH = COMPETITION_DATA_DIR / "data_test_1.csv"
@@ -44,11 +40,14 @@ class CFG:
     num_workers = 8
     on_gpu = True
     test_fragment_id = ['a','b']
-    
-    
+
+
 
 
 '''
+
+# TODO: FINISH TEST DATALOADER, USE INFERENCE NOTEBOOK procedures for submission and predictions
+
 class Vesuvius_Tile_Datamodule(pl.LightningDataModule):
     def __init__(self,
                  cfg,
@@ -58,26 +57,42 @@ class Vesuvius_Tile_Datamodule(pl.LightningDataModule):
 
         self.save_hyperparameters()
         self.cfg = cfg
-        self.train_transform = Image_Transforms.train_transforms
-        self.val_transform  = Image_Transforms.val_transforms
-        self.train_data, self.train_labels = self.get_train_dataset()
-        self.val_data, self.val_labels, self.val_pos = self.get_val_dataset()
 
-        self.train_dataset = Vesuvius_Tile_Datset(images=self.train_data,
-                                                  labels=self.train_labels,
-                                                  transform=self.train_transform)
+        # Get transforms
+        self.train_transform = self.get_transforms('train')
+        self.val_transform = self.get_transforms('val')
+        self.test_transform = self.get_transforms('test')
 
-        self.val_dataset = Vesuvius_Tile_Datset(images=self.val_data,
-                                                labels= self.val_labels,
-                                                transform = self.val_transform)
+        if self.cfg.stage.lower() == 'train':
+            self.train_data, self.train_labels = self.get_train_dataset()
+            self.val_data, self.val_labels, self.val_pos = self.get_val_dataset()
 
 
+            self.train_dataset = Vesuvius_Tile_Datset(images=self.train_data,
+                                                      labels=self.train_labels,
+                                                      transform=self.train_transform)
+
+            self.val_dataset = Vesuvius_Tile_Datset(images=self.val_data,
+                                                    labels=self.val_labels,
+
+
+                                                    transform=self.val_transform)
+
+        # TEST IS NOT FINISHED: DO IT
+        elif self.cfg.stage.lower() == 'test':
+            print('Not implemented, use INFERENCE NOTEBOOK procedures for submission and predictions')
+            #self.test_data, self.test_pos, self.binary_mask = self.get_test_dataset()
+
+
+
+
+    # Data processing is based on https://www.kaggle.com/code/tanakar/2-5d-segmentaion-baseline-training
     def get_train_dataset(self):
         train_images = []
         train_masks = []
 
         for fragment_id in self.cfg.train_fragment_id:
-            image, mask = self.read_image_mask(fragment_id)
+            image, mask = self.read_image_mask('train', fragment_id)
 
             x1_list = list(range(0, image.shape[1] - self.cfg.patch_size + 1, self.cfg.stride))
             y1_list = list(range(0, image.shape[0] - self.cfg.patch_size + 1, self.cfg.stride))
@@ -99,7 +114,7 @@ class Vesuvius_Tile_Datamodule(pl.LightningDataModule):
         valid_xyxys = []
 
         for fragment_id in self.cfg.val_fragment_id:
-            image, mask = self.read_image_mask(fragment_id)
+            image, mask = self.read_image_mask('train', fragment_id)
 
             x1_list = list(range(0, image.shape[1] - self.cfg.patch_size + 1, self.cfg.stride))
             y1_list = list(range(0, image.shape[0] - self.cfg.patch_size + 1, self.cfg.stride))
@@ -117,11 +132,42 @@ class Vesuvius_Tile_Datamodule(pl.LightningDataModule):
 
         return valid_images, valid_masks, valid_xyxys
 
+
+
     def get_test_dataset(self):
+        test_images = []
+        test_xyxys = []
+        binary_masks = []
+
         for fragment_id in self.cfg.val_fragment_id:
-            test_images = self.read_image_test(fragment_id)
+            test_images, binary_mask = self.read_image_mask_test('test',fragment_id)
+            binary_masks.append(binary_mask)
 
-    def read_image_test(self,fragment_id):
+            x1_list = list(range(0, test_images.shape[1] - self.cfg.patch_size + 1, self.cfg.stride))
+            y1_list = list(range(0, test_images.shape[0] - self.cfg.patch_size + 1, self.cfg.stride))
+
+
+
+            for y1 in y1_list:
+                for x1 in x1_list:
+                    y2 = y1 + self.cfg.patch_size
+                    x2 = x1 + self.cfg.patch_size
+
+                    test_images.append(test_images[y1:y2, x1:x2])
+                    test_xyxys.append((x1, y1, x2, y2))
+
+        return test_images, test_xyxys, binary_masks
+
+
+
+
+
+
+
+
+
+    def read_image_mask(self, stage, fragment_id):
+
         images = []
 
         # idxs = range(65)
@@ -131,31 +177,7 @@ class Vesuvius_Tile_Datamodule(pl.LightningDataModule):
         idxs = range(start, end)
 
         for i in tqdm(idxs):
-            image = cv2.imread(COMPETITION_DATA_DIR_str + f"test/{fragment_id}/surface_volume/{i:02}.tif", 0)
-
-            pad0 = (self.cfg.patch_size - image.shape[0] % self.cfg.tile_size)
-            pad1 = (self.cfg.patch_size - image.shape[1] % self.cfg.tile_size)
-
-            image = np.pad(image, [(0, pad0), (0, pad1)], constant_values=0)
-
-            images.append(image)
-        images = np.stack(images, axis=2)
-
-        return images
-
-
-    def read_image_mask(self, fragment_id):
-
-        images = []
-
-        # idxs = range(65)
-        mid = 65 // 2
-        start = mid - self.cfg.z_dim // 2
-        end = mid + self.cfg.z_dim // 2
-        idxs = range(start, end)
-
-        for i in tqdm(idxs):
-            image = cv2.imread(COMPETITION_DATA_DIR_str + f"train/{fragment_id}/surface_volume/{i:02}.tif", 0)
+            image = cv2.imread(self.cfg.competition_data_dir + f"{stage}/{fragment_id}/surface_volume/{i:02}.tif", 0)
 
             pad0 = (self.cfg.patch_size - image.shape[0] % self.cfg.patch_size)
             pad1 = (self.cfg.patch_size - image.shape[1] % self.cfg.patch_size)
@@ -166,13 +188,55 @@ class Vesuvius_Tile_Datamodule(pl.LightningDataModule):
 
         images = np.stack(images, axis=2)
 
-        mask = cv2.imread(COMPETITION_DATA_DIR_str +f"train/{fragment_id}/inklabels.png", 0)
+        mask = cv2.imread(self.cfg.competition_data_dir + f"train/{fragment_id}/inklabels.png", 0)
         mask = np.pad(mask, [(0, pad0), (0, pad1)], constant_values=0)
 
         mask = mask.astype('float32')
         mask /= 255.0
 
         return images, mask
+
+
+    def read_image_mask_test(self, stage, fragment_id):
+
+        images = []
+
+        # idxs = range(65)
+        mid = 65 // 2
+        start = mid - self.cfg.z_dim // 2
+        end = mid + self.cfg.z_dim // 2
+        idxs = range(start, end)
+
+        for i in tqdm(idxs):
+            image = cv2.imread(self.cfg.competition_data_dir + f"{stage}/{fragment_id}/surface_volume/{i:02}.tif", 0)
+
+            pad0 = (self.cfg.patch_size - image.shape[0] % self.cfg.patch_size)
+            pad1 = (self.cfg.patch_size - image.shape[1] % self.cfg.patch_size)
+
+            image = np.pad(image, [(0, pad0), (0, pad1)], constant_values=0)
+
+            images.append(image)
+
+        images = np.stack(images, axis=2)
+
+        binary_mask  = cv2.imread(COMPETITION_DATA_DIR_str + f"test/{fragment_id}/mask.png", 0)
+        binary_mask = (binary_mask / 255).astype(int)
+        pad0 = (self.cfg.patch_size - binary_mask.shape[0] % self.cfg.patch_size)
+        pad1 = (self.cfg.patch_size - binary_mask.shape[1] % self.cfg.patch_size)
+        binary_mask = np.pad(binary_mask, [(0, pad0), (0, pad1)], constant_values=0)
+
+        return images, binary_mask
+
+
+
+    def get_transforms(self, stage):
+        if stage.lower() == 'train':
+            transforms = A.Compose(self.cfg.train_transforms)
+        elif stage.lower() == 'val':
+            transforms = A.Compose(self.cfg.val_transforms)
+        elif stage.lower() == 'test':
+            transforms = A.Compose(self.cfg.test_transforms)
+        return transforms
 
     def train_dataloader(self, *args, **kwargs) -> DataLoader:
         """
@@ -188,9 +252,9 @@ class Vesuvius_Tile_Datamodule(pl.LightningDataModule):
             batch_size=self.cfg.batch_size,
             num_workers=self.cfg.num_workers,
             pin_memory=self.cfg.on_gpu,
-            #multiprocessing_context="spawn"
-            #multiprocessing_context="fork",
-            #collate_fn=self.collate_function,
+            # multiprocessing_context="spawn"
+            # multiprocessing_context="fork",
+            # collate_fn=self.collate_function,
         )
 
     def val_dataloader(self, *args, **kwargs):
@@ -206,19 +270,14 @@ class Vesuvius_Tile_Datamodule(pl.LightningDataModule):
             batch_size=self.cfg.batch_size,
             num_workers=self.cfg.num_workers,
             pin_memory=self.cfg.on_gpu,
-            #multiprocessing_context="spawn"
-            #multiprocessing_context="fork",
-            #collate_fn=self.collate_function
+            # multiprocessing_context="spawn"
+            # multiprocessing_context="fork",
+            # collate_fn=self.collate_function
         )
 
 
-
-
-
-
-
 class Vesuvius_Tile_Datset(Dataset):
-    def __init__(self, images,  labels=None, transform=None):
+    def __init__(self, images, labels=None,  transform=None):
         self.images = images
         self.labels = labels
         self.transform = transform
@@ -239,46 +298,27 @@ class Vesuvius_Tile_Datset(Dataset):
         return image, label
 
 
-class Image_Transforms:
+class Vesuvius_Tile_Datset_TEST(Dataset):
+    def __init__(self, images, test_pos=None, binary_masks =None,  transform=None):
+        self.images = images
+        self.test_pos = test_pos
+        self.binary_masks = binary_masks
+        self.transform = transform
 
-    train_transforms = A.Compose(
-        [
-            # A.RandomResizedCrop(
-            #     size, size, scale=(0.85, 1.0)),
-            A.Resize(PATCH_SIZE, PATCH_SIZE),
-            A.HorizontalFlip(p=0.5),
-            A.VerticalFlip(p=0.5),
-            A.RandomBrightnessContrast(p=0.75),
-            A.ShiftScaleRotate(p=0.75),
-            A.OneOf([
-                A.GaussNoise(var_limit=[10, 50]),
-                A.GaussianBlur(),
-                A.MotionBlur(),
-            ], p=0.4),
-            A.GridDistortion(num_steps=5, distort_limit=0.3, p=0.5),
-            A.CoarseDropout(max_holes=1, max_width=int(PATCH_SIZE * 0.3), max_height=int(PATCH_SIZE * 0.3),
-                            mask_fill_value=0, p=0.5),
-            # A.Cutout(max_h_size=int(size * 0.6),
-            #          max_w_size=int(size * 0.6), num_holes=1, p=1.0),
-            A.Normalize(
-                mean=[0] * Z_DIM,
-                std=[1] * Z_DIM,
-            ),
-            ToTensorV2(transpose_mask=True),
-        ]
-    )
+    def __len__(self):
+        # return len(self.df)
+        return len(self.images)
 
-    val_transforms = A.Compose(
-        [
-        A.Resize(PATCH_SIZE, PATCH_SIZE),
-        A.Normalize(
-            mean=[0] * Z_DIM,
-            std=[1] * Z_DIM
-        ),
+    def __getitem__(self, idx):
+        image = self.images[idx]
+        label = self.labels[idx]
 
-        ToTensorV2(transpose_mask=True),
-    ]
-    )
+        if self.transform:
+            data = self.transform(image=image, mask=label)
+            image = data['image']
+            label = data['mask']
+
+        return image, label
 
 
 
@@ -302,9 +342,9 @@ A.augmentations.geometric.resize.RandomScale(scale_limit=0.1,
                                                 interpolation=1, 
                                                 always_apply=False, 
                                                 p=0.5)
-                                                
-                                                
-                                                
+
+
+
 A.augmentations.geometric.transforms.OpticalDistortion(distort_limit=0.05, 
                                                         shift_limit=0.05, 
                                                         interpolation=1, 
@@ -313,10 +353,10 @@ A.augmentations.geometric.transforms.OpticalDistortion(distort_limit=0.05,
                                                         mask_value=None, 
                                                         always_apply=False, 
                                                         p=0.5)    
-                                                        
-                                                        
-                                                        
-                                                        
+
+
+
+
 A.augmentations.geometric.transforms.ElasticTransform(alpha=1, 
                                                         sigma=50, 
                                                         alpha_affine=50, 
@@ -335,8 +375,8 @@ A.augmentations.geometric.transforms.ElasticTransform(alpha=1,
 
 
 Original Transforms:
-    
-    
+
+
     class Image_Transforms:
 
     train_transforms = A.Compose(
@@ -377,16 +417,16 @@ Original Transforms:
         ToTensorV2(transpose_mask=True),
     ]
     )
-    
-    
-    
-   
+
+
+
+
 
 
 
     Updated:
-    
-    
+
+
     class Image_Transforms:
 
     train_transforms = A.Compose(
@@ -461,5 +501,5 @@ Original Transforms:
     )
 
 
-    
+
     '''
