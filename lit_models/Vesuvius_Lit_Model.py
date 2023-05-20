342c75c88581bd9f3265f7d3b6604de34d1a2136
gregory <txe16@psu.edu>
2023-05-20 14:05:40 -0400

    added new dice and PVT mlp

--- lit_models/Vesuvius_Lit_Model.py
+++ lit_models/Vesuvius_Lit_Model.py
@@ -76,13 +76,13 @@ class Lit_Model(pl.LightningModule):
         self.loss_tversky = smp.losses.TverskyLoss(mode='binary',
                                                  classes=None,
                                                  log_loss=False,
-                                                  from_logits=False,
+                                                  from_logits=True,
                                                    alpha=0.5,
                                                    beta=0.5,
-                                                  gamma=2.0,
-                                                   smooth=1e-05,
+                                                  gamma=1.0,
+                                                   smooth=1e-03,
                                                    ignore_index=None,
-                                                   eps=1e-05,
+                                                   eps=1e-03,
                                                    )
 
 
@@ -120,11 +120,11 @@ class Lit_Model(pl.LightningModule):
 
     def _init_loss(self, y_pred, y_true):
         #return self.loss_bce(y_pred , y_true.float()) + 0.5*self.loss_monai_focal_dice(y_pred , y_true.float() )
-        return self.loss_bce(y_pred , y_true.float())  + self.loss_tversky(y_pred , y_true.float()) #+ 0.5*self.loss_focal(y_pred , y_true.float())
+        #return self.loss_bce(y_pred , y_true.float())  + self.loss_tversky(y_pred , y_true.float()) #+ 0.5*self.loss_focal(y_pred , y_true.float())
         #return self.loss_monai_focal_dice(y_pred , y_true)
         #return self.loss_bce(y_pred , y_true.float()) #+ self.loss_monai_focal_dice(y_pred , y_true.float())
         #return self.loss_bce(y_pred, y_true.float())# + self.loss_monai_focal_dice(y_pred, y_true.float()) #self.dice_kaggle(y_pred, y_true.float())
-        #return  self.loss_bce(y_pred , y_true.float()) + self.dice_new(y_pred, y_true.float())
+        return  self.loss_bce(y_pred , y_true.float()) + self.dice_new(y_pred, y_true.float())
 
     def _init_model(self):
         return self.cfg.model
